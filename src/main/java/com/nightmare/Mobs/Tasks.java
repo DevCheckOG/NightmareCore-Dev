1c37c077dbee43ee065f9c2e2ebcd20b5ce450ef
DevCheck <ing.kevin.benavides@gmail.com>
2024-01-10 14:02:03 -0600

    Se actualizo unos pequeños argumentos de 'Tasks.java'.

--- src/main/java/com/nightmare/Mobs/Tasks.java
+++ src/main/java/com/nightmare/Mobs/Tasks.java
@@ -69,7 +69,7 @@ public final class Tasks {
                                 Player player = (Player) entity;
 
                                 if (player.getGameMode() == GameMode.SURVIVAL)
-                                    creeper.setTarget((Player) entity);
+                                    creeper.setTarget(player);
 
                             }
 
@@ -80,7 +80,7 @@ public final class Tasks {
                                 Player player = (Player) entity;
 
                                 if (player.getGameMode() == GameMode.SURVIVAL)
-                                    creeper.setTarget((Player) player);
+                                    creeper.setTarget(player);
 
                             }
 
